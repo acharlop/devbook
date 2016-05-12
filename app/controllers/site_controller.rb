952d9d48b6ddcfe9667fa5ad9b7a1cc199a98924
Avi Charlop <avicharlop@gmail.com>
2016-05-12 14:18:10 -0400

    Panels being loaded from database

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,2 +1,8 @@
 class SiteController < ApplicationController
+	def index
+		@lang = Language.first
+		@class = @lang.klasses.first
+		@methods = @class.meths
+		@meth = @methods.find_by(name: "#<=>")
+	end
 end
