5ce1bb70713c4e390bbc5b59a9a3e70fd02e7743
Avi Charlop <avicharlop@gmail.com>
2016-05-12 17:00:58 -0400

    Changed jstree name and added sources for searching in the tree

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,6 +1,17 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$(function () { $('#jstree_methods').jstree(); });
+$(function () { 
+	$('#jstree').jstree({
+		//  take from
+		// http://jsfiddle.net/53cvtbv9/1/
+		// https://www.jstree.com/api/#/
+		"search": {
+        "case_insensitive": true,
+        "show_only_matches" : true
+    },
+    "plugins": ["search"]
+	}) 
+});
 
 function update_components (method) {
 	$("div#description-body").html(method.description)
@@ -11,10 +22,10 @@ function update_components (method) {
 }
 
 $(document).ready(function() {
-	$("div#jstree_methods").on('click', '.js-class-select', function(event) {
+	$("div#jstree").on('click', '.js-class-select', function(event) {
 		// console.log("class")
 	})
-	$("div#jstree_methods").on('click', '.js-method-select', function(event) {
+	$("div#jstree").on('click', '.js-method-select', function(event) {
 		event.preventDefault();
 		var button = $(event.currentTarget)
 		var id = button.data("method-id")
